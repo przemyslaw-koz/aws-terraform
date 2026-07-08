1775df6a1e9c7c42a8afbc756e9675ccf59c2297
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-07-08 21:06:30 +0200

    added private bucket creation

---
+++ labs/s3/01-secure-private-bucket/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  default_tags = {
+    Owner        = "PrzemyslawKozlowski"
+    ManagedBy    = "Terraform"
+    Project      = "aws-terraform-lab"
+    Environment  = "lab"
+    Lab          = "vpc/01-public-and-private-subnets"
+    Stack        = var.stack_name
+    AutoShutdown = "true"
+  }
+}
